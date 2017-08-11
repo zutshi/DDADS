f19223e193ebf162166eeab142b21d149c84f8b4
zutshi <aaditya.zutshi@gmail.com>
2017-08-11 15:20:45 -0400

    .

--- modeling/vdp_mosaic.m
+++ modeling/vdp_mosaic.m
@@ -1,13 +1,18 @@
-path = '/home/zutshi/work/projects/DDADS/branches/proto/data/vdp/';
+% w = warning('query','last')
+% id = w.identifier
+% id = 'stats:regress:RankDefDesignMat'
+% warning('off',id)
+
+data_path = '/home/zutshi/work/projects/DDADS/branches/proto/data/vdp/';
 
 %% Train
-load([path 'vdp_data_1e4_t02.mat'])
-X_train = Y_summary(:, 1:2);
-Y_train = Y_summary(:, 3:4);
+data_train = load([data_path 'vdp_data_1e4_t02.mat'], 'Y_summary')
+X_train = data_train.Y_summary(:, 1:2);
+Y_train = data_train.Y_summary(:, 3:4);
 y1_train = Y_train(:,1);
 y2_train = Y_train(:,2);
 
-bound = 1e-0;
+bound = 1e-1;
 [H1, guards1] = genPiecewiseAffineModel(X_train, y1_train, bound);
 [H2, guards2] = genPiecewiseAffineModel(X_train, y2_train, bound);
 
@@ -18,10 +23,10 @@ sqrt(mean((y1_-y1_train).^2))
 sqrt(mean((y2_-y2_train).^2))
 
 %% Test
-load([path 'vdp_data_1e3_t02.mat'])
+data_test = load([data_path 'vdp_data_1e3_t02.mat'], 'Y_summary')
 
-X_test = Y_summary(:, 1:2);
-Y_test = Y_summary(:, 3:4);
+X_test = data_test.Y_summary(:, 1:2);
+Y_test = data_test.Y_summary(:, 3:4);
 y1_test = Y_test(:,1);
 y2_test = Y_test(:,2);
 
